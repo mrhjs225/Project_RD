592a896abc173c76e9007623f597d34e5b748963
unknown <mrhjs225@gmail.com>
2019-03-18 18:23:09 +0900

    archer animation2

---
+++ First 2D Project/Assets/Script/BackgroundControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundControl : MonoBehaviour {
+    // Start is called before the first frame update
+    public Animator[] mBackgrounds;
+    void Start()
+    {
+        FlowControl(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void FlowControl (float speed) {
+        foreach(Animator bg in mBackgrounds)
+            bg.speed = speed;
+    }
+}
